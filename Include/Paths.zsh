449e010f74c04ab95cf3c61b81116546a5bbd555
Carter Allen <CarterA@opt-6.com>
2011-09-10 22:05:30 -0600

    Made rbenv only get run if it, you know, exists.

--- Include/Paths.zsh
+++ Include/Paths.zsh
@@ -1,4 +1,8 @@
 PATH=/usr/local/share/python:/depot_tools/:/usr/local/share/npm/bin:/usr/local/narwhal/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/texbin:/usr/X11/bin:/Users/carterallen/bin/:$HOME/.rbenv/bin
-eval "$(rbenv init -)"
+
+if which rbenv &> /dev/null; then
+    eval "$(rbenv init -)"
+fi
+
 export PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
 NPM_PATH=/usr/local/lib/node
